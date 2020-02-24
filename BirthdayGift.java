01bd45894587a7a913c503ce015e52ab6d336732
JamarcusW <60477746+JamarcusW@users.noreply.github.com>
2020-02-24 13:52:51 -0500

    Add files via upload

--- BirthdayGift.java
+++ BirthdayGift.java
@@ -4,49 +4,51 @@ public class BirthdayGift {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String childName, childAge, toy ;
-		
-		
+
+
 		int age;
-		
+
 		Toy P;
-		
-		 JOptionPane.showMessageDialog(null, "Welcome to the Toy company to choose gifts for young children");
-		 
+
+		JOptionPane.showMessageDialog(null, "Welcome to the Toy company to choose gifts for young children");
+
+		do 
+		{
 		childName = JOptionPane. showInputDialog(null, "What is the childs name?");
-		 
+
 		childAge  = JOptionPane. showInputDialog(null, "What is the childs Age?");
-		 
+
 		age = Integer.parseInt(childAge);	
-		
-		
-		
-		int valid;
-		do {
-			
-			
+
+
+		boolean valid;
+		toy = JOptionPane. showInputDialog(null, "Choose a toy: plushie, blocks or a book.");
+
+		while (toy.equalsIgnoreCase("plushie") == false && toy.equalsIgnoreCase("blocks")== false && toy.equalsIgnoreCase("book")== false) 
+		{
+			JOptionPane.showMessageDialog(null, "error invalid choice");
+
 			toy = JOptionPane. showInputDialog(null, "Choose a toy: plushie, blocks or a book.");
-			
-			if (toy != "plushie" || toy != "book" || toy != "blocks") 
-			{
-				valid = 1;
-			}
-			else 
-			{
-				valid = 2;
-			}
-			
 		}
-		 
-		while (valid == 1);
+		
+		String answer;
 		
 		P = new Toy(toy,age);
-		 
+		if(P.ageOK()== false )
+		{
+			
+		answer = JOptionPane. showInputDialog(null, "This toy is not suitable for a child of this age would you like to continue?");
+			
+		}
+			
+		}
 		
-		JOptionPane.showMessageDialog(null, "The child name is "+ childName +", and their age is " + age +".");
-		 
-		 
+		while(.equals("yes"));
+			JOptionPane.showMessageDialog(null, "The child name is "+ childName +", and their age is " + age +".");
+
+
 
 	}
 
